b29fdc71b5741b903db79946ceb2c757722796e1
varlackc <varlackc@mail.gvsu.edu>
2025-09-28 14:06:49 -0500

    test the set_card method

--- tests/test_card.py
+++ tests/test_card.py
@@ -2,5 +2,9 @@ import unittest
 from card import Card
 
 class TestCard(unittest.TestCase):
-    def set_card(self):
-        self.assertIsNotNone(Card().set_card("Hearts","A"))
+    def test_set_card(self):
+        card_a = Card()
+        card_a.set_card("Hearts", "A")
+        self.assertEqual(card_a.suite, "Hearts")
+    def test_set_card_no_output(self):
+        self.assertIsNone(Card().set_card("Hearts","A"))
