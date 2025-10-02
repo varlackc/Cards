35b9d317ee80ee2d0958b7875677fd468d60d70c
varlackc <varlackc@mail.gvsu.edu>
2025-10-01 23:56:33 -0500

    add a deal method to return card elements from the deck

--- card/deck.py
+++ card/deck.py
@@ -5,13 +5,20 @@ class Deck:
     def create(self):
         suites = ["Spades", "Diamonds", "Clubs", "Hearts"]
         ranks = ["A", "2","3","4","5","6","7","8","9","10","J","Q","K",]
+        card_group = []
+        i = 0
         for suite in suites:
             for rank in ranks:
-                self.pile.append(Card().set_card(suite, rank))
+                #card_a.set_card(suite, rank)
+                self.pile.append(None)
+                self.pile[i] = Card()
+                self.pile[i].set_card(suite, rank)
+                i += 1
     # shuffle
 
     # deal
-    
+    def deal(self):
+        return self.pile.pop(0)
     # __len__
     
     # __repr__ Deck of # cards
