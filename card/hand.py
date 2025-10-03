8121c961e8665471e20f38ea2b5053abec04c220
varlackc <varlackc@mail.gvsu.edu>
2025-10-03 15:24:33 -0500

    include a method to add cards into a hand of cards

--- card/hand.py
+++ card/hand.py
@@ -3,3 +3,8 @@ from card import Card
 class Hand:
     def __init__(self):
         self.cards = []
+        self.hand_size = 5
+    def add_card(self, card_a):
+        self.cards.append(card_a)
+        while len(self.cards) > self.hand_size:
+            self.cards[0]
