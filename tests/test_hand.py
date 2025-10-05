8bae2177ec8e63e44304565b09ce61bab64b176e
varlackc <varlackc@mail.gvsu.edu>
2025-10-05 18:53:03 -0500

    add test to ensure that a card can be added to the hand

--- tests/test_hand.py
+++ tests/test_hand.py
@@ -1,4 +1,11 @@
 import unittest
-from card import Hand
+from card import Card, Hand
 
 class TestHand(unittest.TestCase):
+    def test_hand_add_card(self):
+        card_a = Card()
+        hand_a = Hand()
+        card_a.set_card("Hearts", "A")
+        hand_a.add_card(card_a)
+        self.assertEqual(len(hand_a.cards), 1)
+        
