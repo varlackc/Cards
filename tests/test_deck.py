0613614a48d5393748f3592c9f95b9496be419bc
varlackc <varlackc@mail.gvsu.edu>
2025-10-04 21:12:16 -0500

    add initial tests to the test_deck

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -1,4 +1,9 @@
 import unittest
+from card import Deck
 
 class TestDeck(unittest.TestCase):
-    
+    def test_create(self):
+        deck_a = Deck()
+        deck_a.create()
+        self.assertEqual(len(deck_a.pile), 52)
+        
