ef13b833d3a508202eb460b442d9d7e27f8894c4
varlackc <varlackc@mail.gvsu.edu>
2025-10-05 19:19:12 -0500

    Add test to verify that the deck can deal a card

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -6,4 +6,8 @@ class TestDeck(unittest.TestCase):
         deck_a = Deck()
         deck_a.create()
         self.assertEqual(len(deck_a.pile), 52)
+    def test_deal(self):
+        deck_a = Deck()
+        deck_a.create()
+        self.assertIsNotNone(deck_a.deal())
         
