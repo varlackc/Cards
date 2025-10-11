2ea158fa685ca382a4a03d64706ae173a7d0f206
varlackc <varlackc@mail.gvsu.edu>
2025-10-10 23:39:46 -0500

    add a test_add to the discard test

--- tests/test_discard.py
+++ tests/test_discard.py
@@ -3,3 +3,9 @@ from card import discard
 from card import card
 
 class TestDiscard(unittest.TestCase):
+    def test_add(self):
+        card_a = card()
+        card_a.set_card("Hearts", "A")
+        discard_a = discard()
+        discard_a.add(card_a)
+        self.assertIsNotNone(discard_a.pile)
