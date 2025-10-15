044b7c35981fdf2381eda37829dae41c7c710bfd
varlackc <varlackc@mail.gvsu.edu>
2025-10-14 23:51:41 -0500

    add space to the card class
    add space to the card class

--- card/card.py
+++ card/card.py
@@ -2,14 +2,19 @@ class Card:
     def __init__(self):
         self.suite = ""
         self.rank = ""
+        
     def get_card(self):
         return (self.suite, self.rank)
+    
     def set_card(self,suite,rank):
         self.suite = suite
         self.rank = rank
+        
     def get_suite(self):
-        return self.suite 
+        return self.suite
+    
     def get_rank(self):
         return self.rank
+    
     def __repr__(self):
         return f"{self.rank} of {self.suite}"
