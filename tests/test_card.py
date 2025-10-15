ec7e4b9a3de4181a42d0be4a3ce3f5fe3828b269
varlackc <varlackc@mail.gvsu.edu>
2025-10-14 23:47:11 -0500

    add spacing between methods in the test_card class
    add spacing between methods in the test_card class

--- tests/test_card.py
+++ tests/test_card.py
@@ -6,32 +6,40 @@ class TestCard(unittest.TestCase):
         card_a = Card()
         card_a.set_card("Hearts", "A")
         self.assertEqual(card_a.suite, "Hearts")
+        
     def test_set_card_no_output(self):
         self.assertIsNone(Card().set_card("Hearts","A"))
+        
     def test_get_card_output(self):
         card_a = Card()
         card_a.set_card("Hearts", "A")
         self.assertIsNotNone(card_a.get_card())
+        
     def test_get_card(self):
         card_a = Card()
         card_a.set_card("Hearts", "A")
         self.assertEqual(card_a.get_card(), ('Hearts', 'A'))
+        
     def test_get_suite_output(self):
         card_a = Card()
         card_a.set_card("Hearts", "A")
         self.assertIsNotNone(card_a.get_suite())
+        
     def test_get_suite(self):
         card_a = Card()
         card_a.set_card("Hearts", "A")
         self.assertEqual(card_a.get_suite(), "Hearts")
+        
     def test_get_rank_output(self):
         card_a = Card()
         card_a.set_card("Hearts", "A")
         self.assertIsNotNone(card_a.get_rank(), "A")
+        
     def test_print_output(self):
         card_a = Card()
         card_a.set_card("Hearts", "A")
         self.assertIsNotNone(card_a.__repr__())
+        
     def test_print(self):
         card_a = Card()
         card_a.set_card("Hearts", "A")
