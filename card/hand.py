e7623d7fb08773cacad107b2bee1310c31db3d62
varlackc <varlackc@mail.gvsu.edu>
2025-11-24 23:52:49 -0600

    update type hints in the add_card method in the hand class

--- card/hand.py
+++ card/hand.py
@@ -8,11 +8,11 @@ from card import Card
 
 
 class Hand:
-    def __init__(self):
+    def __init__(self) -> None:
         self.cards = []
         self.hand_size = 5
         
-    def add_card(self, card_a):
+    def add_card(self, card_a) -> None:
         self.cards.append(card_a)
         while len(self.cards) > self.hand_size:
             self.cards[0]
