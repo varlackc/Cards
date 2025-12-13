0bccaa30a2d6f9abd9a2da9a2301bb3df4c56645
varlackc <varlackc@mail.gvsu.edu>
2025-12-12 23:59:07 -0600

    add comment to the add_card method

--- card/hand.py
+++ card/hand.py
@@ -16,6 +16,9 @@ class Hand:
         self.hand_size = 5
         
     def add_card(self, card_a) -> None:
+        """
+        Method to add a card to the hand
+        """
         self.cards.append(card_a)
         while len(self.cards) > self.hand_size:
             self.cards[0]
