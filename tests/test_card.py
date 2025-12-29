b02e261cb0df53a300ca1cf9de58d4f82b9e2b94
varlackc <varlackc@mail.gvsu.edu>
2025-12-29 09:45:19 -0600

    add a comment to the print_output method

--- tests/test_card.py
+++ tests/test_card.py
@@ -70,6 +70,10 @@ class TestCard(unittest.TestCase):
         self.assertIsNotNone(card_a.get_rank(), "A")
         
     def test_print_output(self) -> None:
+        """
+        Test to verify the test_print_output method
+        :param self: Description
+        """
         card_a = Card()
         card_a.set_card("Hearts", "A")
         self.assertIsNotNone(card_a.__repr__())
